36f232d80544bc9e5ed7e288fe6e15e9dc6eedc1
RomanFuji <Ramenm1001@gmail.com>
2022-06-17 19:45:06 +1000

    blue

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import turtle as t
 t.color("red")
+t.speed(10)
 for i in range(12):
     t.forward(100)
     t.right(30)
@@ -8,5 +9,10 @@ t.color("salmon")
 for i in range(99):
     t.forward(i)
     t.left(89)
+
+t.color("blue")
+for i in range(12):
+    t.forward(10)
+    t.right(30)
 
 t.mainloop()
